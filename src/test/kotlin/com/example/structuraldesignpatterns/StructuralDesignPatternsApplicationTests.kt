35d6c0f068de30855e07069c1a789cb631ea8c65
Vinicius R <vrodrigues.code@gmail.com>
2022-10-07 08:08:37 -0300

    spring initialzr

---
+++ src/test/kotlin/com/example/structuraldesignpatterns/StructuralDesignPatternsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.structuraldesignpatterns
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StructuralDesignPatternsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
